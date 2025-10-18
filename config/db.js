ce4585852e4c7015e1d4b79ead48ca44df786517
Klodi <mekshiklodi10@gmail.com>
2025-10-18 15:36:28 +0200

    ENV

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+// db.js
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
@@ -5,9 +6,13 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;

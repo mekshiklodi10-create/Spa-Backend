373c04a7b2d8abc337eee1c1416b441bf95cfa25
Klodi <mekshiklodi10@gmail.com>
2026-01-04 13:18:53 +0100

    Klod

--- config/db.js
+++ config/db.js
@@ -1,23 +1,12 @@
-// db.js
-import mysql from "mysql2/promise";
+
+import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY 
+);
 
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  
-});
-
-export default pool;
+export default supabase;
